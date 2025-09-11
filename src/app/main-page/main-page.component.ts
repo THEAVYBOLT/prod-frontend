a9224eff94763b5ad5dce9a7f124735edc20a997
THEAVYBOLT <yashtiwariayv7798@gmail.com>
2025-09-11 23:31:58 +0530

    integrate Angular Material for theming and icons, update styles and main page component accordingly

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {TopicSuggestionsComponent} from '../topic-suggestions/topic-suggestions.component';
 import {TopicsSummaryPageComponent} from '../topics-summary-page/topics-summary-page.component';
 import {MenuBarComponent} from '../menu-bar/menu-bar.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-main-page',
   imports: [
     TopicSuggestionsComponent,
     TopicsSummaryPageComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    MatIconModule
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
